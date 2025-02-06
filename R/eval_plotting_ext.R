35723c5ca5a7e5c3ad10966817743abdcc9205ed
Floris Padt <floris@fmpadt.nl>
2025-02-06 22:19:24 +0100

    color scale convience

--- R/eval_plotting_ext.R
+++ R/eval_plotting_ext.R
@@ -37,7 +37,7 @@ pa_palette_get <- function(
 #' @param .name_or_index Either a string (name of the color) or a numeric index.
 #' @return A string with the color hex code.
 #' @export
-pa_color_get <- function(
+pa_brand_color_get <- function(
     .name_or_index) {
 
   if (is.character(.name_or_index)) {
@@ -65,16 +65,42 @@ pa_brand_theme <- function() {
     )
 }
 
-#' A ggplot2 theme that applies brand styling from the YAML config.
-#' This theme is suitable for plots with a white background.
-#'
-#' @param ... Additional arguments to pass to ggplot2::theme().
-#' @return A ggplot2 theme object.
-#' @export
-pa_color_brand_scale <- function(...) {
-  palette <- pa_config_value_get("theme.color_palette")
-  ggplot2::scale_color_manual(values = palette, ...)
-}
 
+#' Custom scale_color_manual using brand colors from YAML
+#'
+#' This function retrieves colors from the brand color palette in the YAML configuration
+#' and applies them to ggplot2's scale_color_manual() based on the number of labels.
+#'
+#' @param labels Character vector of labels to be used in the legend.
+#' @return A ggplot2 scale_color_manual object.
+#' @examples
+#' \dontrun{
+#' ggplot(data, aes(x, y, color = group)) +
+#'   geom_line() +
+#'   scale_color_brand(labels = c("M-1", "M-2", "M-3"))
+#'   }
+#' @export
+pa_brand_color_scale <- function(labels) {
+
+  # Retrieve the color palette from YAML
+  brand_palette <- pa_config_value_get("theme.color_palette")
+
+  # Ensure YAML config is available
+  if (is.null(brand_palette)) {
+    stop("Brand colors are not defined in the YAML configuration.")
+  }
+
+  # Ensure the required number of colors are available
+  num_labels <- length(labels)
+  if (num_labels > length(brand_palette)) {
+    stop("Not enough brand colors defined in YAML for the number of labels.")
+  }
+
+  # Select the required number of colors
+  selected_colors <- brand_palette[1:num_labels]
+
+  # Apply scale_color_manual() with the selected colors
+  ggplot2::scale_color_manual(labels = labels, values = selected_colors)
+}
 
 
